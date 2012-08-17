e9133c3906f8d9387efecce4f842d07945cb7cfb
Tom De Vylder <tom@penumbra.be>
2012-08-17 15:00:24 +0200

    Add refactor warning

--- manifests/user.pp
+++ manifests/user.pp
@@ -4,6 +4,14 @@ define percona::user (
   $ensure = present,
   $host   = 'localhost'
 ) {
+
+  #
+  # WARNING:
+  # Needs refactoring in order to work on any non-Debian distro.
+  # Better yet user.pp should be split to a separate module which can
+  # be used by other MySQL distro's as well.
+  #
+
   case $ensure {
     'present': {
       exec { "MySQL: create user ${name}":
