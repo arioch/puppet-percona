bca2d89d72a1ab6fa0dafaa4d051705948bfd081
Tom De Vylder <tom@penumbra.be>
2012-08-17 12:57:16 +0200

    Bugfix: rspec no longer fails on aptkey

--- spec/classes/percona_preinstall_spec.rb
+++ spec/classes/percona_preinstall_spec.rb
@@ -6,20 +6,15 @@ describe 'percona::preinstall' do
     let(:node) { 'percona' }
     let(:facts) { {:operatingsystem => 'Debian', :kernel => 'Linux'} }
 
-    it { should create_class("percona::preinstall") }
-
-    it { should contain_exec('apt-get update')\
-      .with_command("apt-get update")\
-      .with_refreshonly(true)
-    }
-
-    it { should contain_apt__key('CD2EFD2A')\
-      .with_notify(["Exec[apt-get update]"])
-    }
-
-    it { should contain_apt__sources_list('percona')\
-      .with_ensure('present')
-    }
+    it do
+      should create_class("percona::preinstall")
+      should contain_exec('apt-get update')\
+        .with_command("apt-get update")\
+        .with_refreshonly(true)
+      should contain_apt__key('CD2EFD2A')
+      should contain_apt__sources_list('percona')\
+        .with_ensure('present')
+    end
   end
 
   describe "[CentOS] percona::preinstall class" do
@@ -27,7 +22,9 @@ describe 'percona::preinstall' do
     let(:node) { 'percona' }
     let(:facts) { {:operatingsystem => 'CentOS', :kernel => 'Linux'} }
 
-    it { should create_class("percona::preinstall") }
-    it { should contain_package('percona-release').with_ensure('present') }
+    it do
+      should create_class("percona::preinstall")
+      should contain_package('percona-release').with_ensure('present')
+    end
   end
 end
