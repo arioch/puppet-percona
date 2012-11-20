caf3ae39beabd64a19143fcfce2bdb84da2be48f
Jan Vansteenkiste <jan@vstone.eu>
2012-11-20 22:31:58 +0100

    Added a test for the mysql_password function

---
+++ spec/functions/mysql_password_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'the mysql_password function' do
+
+  let(:scope) { Puppet::Parser::Scope.new }
+
+  it 'should exist' do
+    Puppet::Parser::Functions.function('mysql_password').should == "function_mysql_password"
+  end
+
+  it 'should convert a password to a hash' do
+    password = 'mypass'
+    scope.function_mysql_password([password]).should == '*6C8989366EAF75BB670AD8EA7A7FC1176A95CEF4'
+  end
+
+end
