43d7a9c06d2521a6b1d7413383138ab150a79a59
Tom De Vylder <tom@penumbra.be>
2012-11-29 11:38:54 +0100

    Lintian fixes

--- manifests/config/server.pp
+++ manifests/config/server.pp
@@ -5,13 +5,13 @@
 # TODO: Document class.
 #
 class percona::config::server {
-  $config_content   = $::percona::config_content
-  $config_dir       = $::percona::config_dir
-  $config_dir_mode  = $::percona::config_dir_mode
-  $config_file      = $::percona::config_file
-  $config_file_mode = $::percona::config_file_mode
-  $config_group     = $::percona::config_group
-  $config_user      = $::percona::config_user
+  $config_content    = $::percona::config_content
+  $config_dir        = $::percona::config_dir
+  $config_dir_mode   = $::percona::config_dir_mode
+  $config_file       = $::percona::config_file
+  $config_file_mode  = $::percona::config_file_mode
+  $config_group      = $::percona::config_group
+  $config_user       = $::percona::config_user
   $config_includedir = $::percona::config_includedir
 
   $config_skip      = $::percona::config_skip
