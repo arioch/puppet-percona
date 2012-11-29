b12d57da0f624668299391bfa2b6d94d75ec4878
Tom De Vylder <tom@penumbra.be>
2012-11-29 12:51:26 +0100

    Make it puppetlabs/apt compatible

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -7,12 +7,26 @@ class percona::repo::apt {
     notify => Exec['percona::repo::apt-get update'],
   }
 
-  apt::sources_list { 'percona':
-    ensure  => present,
-    source  => false,
-    content => template ("${module_name}/repo/sources.list.erb"),
-    require => Apt::Key['CD2EFD2A'],
-    notify  => Exec['percona::repo::apt-get update'],
+  if defined('apt::sources_list') {
+    # Camp2Camp/apt module
+    apt::sources_list { 'percona':
+      ensure  => present,
+      source  => false,
+      content => template ("${module_name}/repo/sources.list.erb"),
+      notify  => Exec['percona::repo::apt-get update'],
+    }
+  }
+
+  if defined('apt::source') {
+    # Puppetlabs/apt module
+    apt::source { 'percona':
+      ensure      => present,
+      include_src => true,
+      location    => 'http://repo.percona.com/apt',
+      release     => $::lsbdistcodename,
+      repos       => 'main',
+      notify      => Exec['percona::repo::apt-get update'],
+    }
   }
 
   exec { 'percona::repo::apt-get update':
