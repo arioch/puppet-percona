c1887f3e2ab98f4b0917148d41b337d2c271fb0f
John Moser <john.r.moser@gmail.com>
2013-01-16 04:34:48 -0500

    inodb_autoinc_lock_mode must be 2 for multiple slave threads in cluster

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -15,13 +15,14 @@
 class percona::cluster {
   if ( $percona::cluster ) {
     $params = {
-      'mysqld/server_id'             => $percona::cluster_index,
-      'mysqld/wsrep_cluster_address' => $percona::cluster_address,
-      'mysqld/wsrep_node_name'       => $percona::cluster_node_name,
-      'mysqld/wsrep_cluster_name'    => $percona::cluster_name,
-      'mysqld/wsrep_slave_threads'   => $percona::cluster_slave_threads,
-      'mysqld/wsrep_sst_method'      => $percona::cluster_sst_method,
-      'mysqld/wsrep_provider'        => $percona::cluster_wsrep_provider,
+      'mysqld/server_id'                => $percona::cluster_index,
+      'mysqld/wsrep_cluster_address'    => $percona::cluster_address,
+      'mysqld/wsrep_node_name'          => $percona::cluster_node_name,
+      'mysqld/wsrep_cluster_name'       => $percona::cluster_name,
+      'mysqld/wsrep_slave_threads'      => $percona::cluster_slave_threads,
+      'mysqld/wsrep_sst_method'         => $percona::cluster_sst_method,
+      'mysqld/wsrep_provider'           => $percona::cluster_wsrep_provider,
+      'mysqld/innodb_autoinc_lock_mode' => '2',
     }
   }
 }
